acbce5e78b7942880819b67dc92630dcd2902bc6
daiyankhann <daiyanhudakhan@gmail.com>
2025-03-10 00:17:31 +0530

    Update Divisible_by_7.java

--- Assignment 1/Divisible_by_7.java
+++ Assignment 1/Divisible_by_7.java
@@ -1,20 +1,26 @@
 class SumDivisibleBy7 {
-    void Display_Sum() {
-        int sum = 0;
+
+    // Method to display numbers divisible by 7 and their sum
+    void displaySum() {
+        int sumOfDivisibleNumbers = 0;
         System.out.println("Numbers divisible by 7 between 100 and 200 are:");
-        for (int i = 101; i < 200; i++) {
-            if (i % 7 == 0) {
-                System.out.print(i + " ");
-                sum += i;
+        
+        // Loop through numbers from 101 to 199
+        for (int currentNumber = 101; currentNumber < 200; currentNumber++) {
+            if (currentNumber % 7 == 0) {
+                System.out.print(currentNumber + " "); // Print the divisible number
+                sumOfDivisibleNumbers += currentNumber; // Add to the sum
             }
         }
-        System.out.println("\nSum of all numbers divisible by 7 is: " + sum);
+        
+        // Print the total sum
+        System.out.println("\nSum of all numbers divisible by 7 is: " + sumOfDivisibleNumbers);
     }
 }
 
-class Divisible_by_7 {
+public class DivisibleBy7 {
     public static void main(String[] args) {
-        SumDivisibleBy7 obj = new SumDivisibleBy7();
-        obj.Display_Sum();
+        SumDivisibleBy7 calculator = new SumDivisibleBy7(); // Create object
+        calculator.displaySum(); // Call the method
     }
 }
